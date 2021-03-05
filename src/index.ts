69a5cc45502ffdf088ba4ca180080a3ab17e330c
Niki <nikimortazavi21@gmail.com>
2021-03-05 13:18:49 +0200

    MikroORM TypeGraphQL Crud

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
+import "reflect-metadata"
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/posts";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: "my first port" });
-  // await orm.em.persistAndFlush(post);
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts)
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      validate: false,
+      resolvers: [HelloResolver, PostResolver],
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server starting on localhost:4000");
+  });
 };
 
 main().catch((err) => {
