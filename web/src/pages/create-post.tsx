9eaebec6b5a7502eaea745380768d9219caadf3e
Niki <nikimortazavi21@gmail.com>
2021-03-24 19:34:15 +0200

    update routing

--- web/src/pages/create-post.tsx
+++ web/src/pages/create-post.tsx
@@ -1,26 +1,18 @@
 import { Box, Button } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
 import React from "react";
 import { InputField } from "../components/InputField";
-import { Wrapper } from "../components/Wrapper";
-import { useCreatePostMutation, useMeQuery } from "../generated/graphql";
-import { useRouter } from "next/router";
-import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
 import { Layout } from "../components/Layout";
-import { useEffect } from "react";
+import { useCreatePostMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { useIsAuth } from "../utils/useIsAuth";
 
 const CreatePost: React.FC<{}> = ({}) => {
-  const [{ data, fetching }] = useMeQuery();
   const router = useRouter();
+  useIsAuth();
   const [, createPost] = useCreatePostMutation();
-
-  useEffect(() => {
-    if (!fetching && !data?.me) {
-      router.replace("/login");
-    }
-  }, [data, router]);
-
   return (
     <Layout variant="small">
       <Formik
