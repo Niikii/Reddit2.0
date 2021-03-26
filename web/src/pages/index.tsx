ac1bdc50b6b42c0712470c9096f61c50f0fe201f
Niki <nikimortazavi21@gmail.com>
2021-03-26 11:05:06 +0200

    post ui update

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,31 +1,59 @@
-import { ssrExchange } from "@urql/core";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
+import NextLink from "next/link";
 import React from "react";
 import { Layout } from "../components/Layout";
-import { NavBar } from "../components/NavBar";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
-import NextLink from "next/link";
-import { Link } from "@chakra-ui/react";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
     },
   });
+
+  if (!fetching && !data) {
+    return <div>query failed for some reason</div>;
+  }
+
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>Create post</Link>
-      </NextLink>
+      <Flex align="center">
+        <Heading>MyReddit:)</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">Create post</Link>
+        </NextLink>
+      </Flex>
 
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>louding...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={8}>
+          {data!.posts.map((p) => (
+            <Box p={p.id} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}...</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
+      {data ? (
+        <Flex>
+          <Button isLoading={fetching} m="auto" my={8}>
+            load more
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
