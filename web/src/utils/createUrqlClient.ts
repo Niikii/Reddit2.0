1e742e92ce1be51d61d42d404f36f78782f6100f
Niki <nikimortazavi21@gmail.com>
2021-03-29 11:06:20 +0200

    pagination checking for more posts

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -1,5 +1,6 @@
 import { cacheExchange, Resolver } from "@urql/exchange-graphcache";
 import Router from "next/router";
+import { cachedDataVersionTag } from "node:v8";
 import {
   dedupExchange,
   Exchange,
@@ -27,11 +28,12 @@ const errorExchange: Exchange = ({ forward }) => (ops$) => {
   );
 };
 
+//pagination
 const cursorPagination = (): Resolver => {
   return (_parent, fieldArgs, cache, info) => {
     const { parentKey: entityKey, fieldName } = info;
     const allFields = cache.inspectFields(entityKey);
-    console.log("all fileds", allFields)
+    console.log("all fileds", allFields);
     const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
     const size = fieldInfos.length;
     if (size === 0) {
@@ -39,15 +41,28 @@ const cursorPagination = (): Resolver => {
     }
 
     const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
-    const isItInTheCache = cache.resolve(entityKey, fieldKey) as string[];
+    const isItInTheCache = cache.resolve(
+      cache.resolve(entityKey, fieldKey) as string,
+      "posts"
+    );
     info.partial = !isItInTheCache;
+    let hasMore = true;
     const results: string[] = [];
     fieldInfos.forEach((fi) => {
-      const data = cache.resolve(entityKey, fi.fieldKey) as string[];
+      const key = cache.resolve(entityKey, fi.fieldKey) as string;
+      const data = cache.resolve(key, "posts") as string[];
+      const _hasMore = cache.resolve(key, "hasMore");
+      if (!_hasMore) {
+        hasMore = _hasMore as boolean;
+      }
       results.push(...data);
     });
 
-    return results;
+    return {
+      __typename: "PaginatedPosts",
+      hasMore,
+      posts: results,
+    };
 
     //     const visited = new Set();
     //     let result: NullArray<string> = [];
@@ -100,7 +115,6 @@ const cursorPagination = (): Resolver => {
     //       info.partial = true;
     //       return result;
     //     }
-    console.log("hello there");
   };
 };
 
@@ -112,6 +126,9 @@ export const createUrqlClient = (ssrExchange: any) => ({
   exchanges: [
     dedupExchange,
     cacheExchange({
+      keys: {
+        PaginatedPosts: () => null,
+      },
       resolvers: {
         query: {
           posts: cursorPagination(),
